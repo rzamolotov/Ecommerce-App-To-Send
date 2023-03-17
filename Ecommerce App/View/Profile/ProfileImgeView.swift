16357703af3f835cbe2673b94801dcb449c4dc78
Roman Zamolotov <rzamolotov@gmail.com>
2023-03-17 13:30:36 +0300

    Upload Item

--- Ecommerce App/View/Profile/ProfileImgeView.swift
+++ Ecommerce App/View/Profile/ProfileImgeView.swift
@@ -11,20 +11,29 @@ struct ProfileImgeView: View {
     
     var body: some View {
         VStack{
-            Image("avatar")
-                .resizable()
-                .scaledToFill()
-                .clipShape(Circle())
-                .frame(width: 60, height: 60)
-         
-
+            Button {
+                
+            } label: {
+                Image("avatar")
+                    .resizable()
+                    .scaledToFill()
+                    .clipShape(Circle())
+                    .frame(width: 60, height: 60)
+            }
+            .padding(.bottom, 2)
             
             Button {
                 
             } label: {
                 Text("Change photo")
+                    .foregroundColor(grayFontColor)
+                    .font(.custom(lightFont, size: 10))
             }
+            .padding(.bottom, 15)
+            
             Text("Satria Adhi Pranada")
+                .foregroundColor(blackColor)
+                .font(.custom(boldFont, size: 20))
             
         }
     }
---
+++ Ecommerce App/View/Profile/UploadItem.swift
@@ -0,0 +1,35 @@
+//
+//  UploadItem.swift
+//  Ecommerce App
+//
+//  Created by Роман Замолотов on 17.03.2023.
+//
+
+import SwiftUI
+
+struct UploadItem: View {
+    var body: some View {
+        ZStack{
+            Rectangle()
+                .frame(width: 289, height: 46)
+                .foregroundColor(violetColor)
+                .cornerRadius(15.0)
+            HStack{
+                Image(systemName: "square.and.arrow.up")
+                    .foregroundColor(.white)
+                    .font(.custom(boldFont, size: 14))
+                    .padding(.leading, -30)
+                Text("Upload Item")
+                    .font(.custom(boldFont, size: 14))
+                    .foregroundColor(.white)
+                    .padding()
+            }
+        }
+    }
+}
+
+struct UploadItem_Previews: PreviewProvider {
+    static var previews: some View {
+        UploadItem()
+    }
+}
