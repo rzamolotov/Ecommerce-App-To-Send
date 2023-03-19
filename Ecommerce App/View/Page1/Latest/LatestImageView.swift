e84d9c7c19626fce71dc204bc6abaf6e2361396a
Roman Zamolotov <rzamolotov@gmail.com>
2023-03-19 22:29:37 +0300

    Latest View1

--- Ecommerce App/View/Page1/Latest/LatestImageView.swift
+++ Ecommerce App/View/Page1/Latest/LatestImageView.swift
@@ -13,48 +13,48 @@ struct LatestImageView: View {
             
         } label: {
             ZStack{
-                Image("phoneImage")
+                Image("sneakers")
                     .resizable()
                     .scaledToFill()
-                VStack {
-                    Spacer()
-                    HStack {
-                        VStack(alignment: .leading, spacing: 3) {
-                            ZStack{
-                                Rectangle()
-                                    .frame(width: 35, height: 12)
-                                    .cornerRadius(15)
-                                    .foregroundColor(grayBackgroundColor)
-                                    .opacity(0.5)
-                                Text("Phones")
-                                    .font(.custom(mediumFont, size: 5))
-                            }
-                            Text("Samsung")
-                                .foregroundColor(.white)
-                                .font(.custom(boldFont, size: 10))
-                        }
-                        Spacer()
-                    }
-                    .padding(.leading)
-                    .padding(.top)
-                    .padding(.bottom, -20)
-                    
+                
+                VStack(alignment: .leading) {
                     HStack{
-                        //TODO: add if else
-                        Text("console")
-                            .font(.custom(boldFont, size: 9))
-                            .foregroundColor(.white)
-                            .padding(.leading)
-                            .padding(.top, 15)
+                        Image("face")
                         Spacer()
+                        ZStack{
+                            Rectangle()
+                                .frame(width: 49, height: 18)
+                                .cornerRadius(15)
+                                .foregroundColor(.red)
+                            Text("30% off")
+                                .font(.custom(mediumFont, size: 7))
+                                .foregroundColor(.white)
+                        }
+                    }//: HStack face+sale
+                    Spacer()
+                    ZStack{
+                        Rectangle()
+                            .frame(width: 50, height: 17)
+                            .cornerRadius(15)
+                            .foregroundColor(grayBackgroundColor)
+                            .opacity(0.6)
+                        Text("kids")
+                            .font(.custom(semiBoldFont, size: 9))
+                            .foregroundColor(blackColor)
                     }
                     
+                    Text("New ballance")
+                        .foregroundColor(.white)
+                        .font(.custom(boldFont, size: 13))
+                        .padding(.bottom, 0)
+                    Text("Sneakers")
+                        .font(.custom(boldFont, size: 13))
+                        .foregroundColor(.white)
+                        .padding(.bottom, -3)
                     HStack {
-                        Text("$ 180,000")
+                        Text("$ 33,00")
                             .foregroundColor(.white)
-                            .font(.custom(boldFont, size: 8))
-                            .padding(.leading)
-                            .padding(.top, 15)
+                            .font(.custom(boldFont, size: 10))
                         
                         Spacer()
                         
@@ -62,17 +62,28 @@ struct LatestImageView: View {
                             ZStack {
                                 Circle()
                                     .foregroundColor(grayBackgroundColor)
+                                Image("heart1")
+                            }
+                            .frame(width: 28, height: 28)
+                        }
+                        
+                        Button { } label: {
+                            ZStack {
+                                Circle()
+                                    .foregroundColor(grayBackgroundColor)
                                 Image(systemName: "plus")
                             }
-                            .frame(width: 20, height: 20)
+                            .frame(width: 35, height: 35)
                         }
-                        .padding()
-                    }
-                    .padding(.top, -35)
-                }
-            }
-            .frame(width: 114, height: 149)
-        }
+                    }//: HStack price/plus/heart
+                }//: VStack
+                .padding(.leading, 10)
+                .padding(.trailing, 10)
+                .padding(.bottom, 5)
+                .padding(.top, 8)
+            }//:ZStack
+            .frame(width: 174, height: 221)
+        }//:Button label
     }
 }
 
